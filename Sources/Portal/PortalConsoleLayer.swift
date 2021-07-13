b80ec7cb9ee083395f89a1710da49dfc6df3e244
Ben Gottlieb <ben@standalone.com>
2021-07-13 14:13:37 -0500

    added new PortalConsoleLayer view

---
+++ Sources/Portal/PortalConsoleLayer.swift
@@ -0,0 +1,54 @@
+//
+//  PortalConsoleLayer.swift
+//  
+//
+//  Created by Ben Gottlieb on 7/13/21.
+//
+
+import SwiftUI
+
+@available(iOS 14.0, watchOS 7.0, *)
+class ConsoleManager: ObservableObject {
+	static let instance = ConsoleManager()
+	
+	@Published var showConsole = false
+}
+
+@available(iOS 14.0, watchOS 7.0, *)
+public struct PortalConsoleLayer: View {
+	@State var yOffset: CGFloat = 0
+	@State var dragOffset: CGFloat = 0
+	
+	public static var showConsole: Bool {
+		get { ConsoleManager.instance.showConsole }
+		set { ConsoleManager.instance.showConsole = newValue }
+	}
+	
+	@ObservedObject var manager = ConsoleManager.instance
+	
+	public init() {
+		
+	}
+	
+	public var body: some View {
+		if ConsoleManager.instance.showConsole {
+			ZStack() {
+				Color.clear
+				
+				VStack() {
+					Spacer()
+					
+					PortalConsoleView()
+						.padding()
+						.gesture(DragGesture(minimumDistance: 0, coordinateSpace: .global).onChanged { value in
+							dragOffset = value.translation.height
+						}.onEnded { value in
+							yOffset += dragOffset
+							dragOffset = 0
+						})
+						.offset(y: yOffset + dragOffset)
+				}
+			}
+		}
+	}
+}
