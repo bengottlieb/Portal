eab749b504a3b806915d9f6af41d115457f52ea8
Ben Gottlieb <ben@standalone.com>
2021-09-18 17:33:41 -0500

    updated for latest swift-tools

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Portal",
 	 platforms: [
-			  .iOS(.v12),
-			  .watchOS(.v5)
+			  .iOS(.v14),
+			  .watchOS(.v7)
 		 ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["Portal"]),
     ],
 	dependencies: [
-	  .package(url: "https://github.com/bengottlieb/Suite.git", from: "0.10.85"),
+	  .package(url: "https://github.com/bengottlieb/Suite.git", from: "1.0.0"),
 
 	],
     targets: [
@@ -24,6 +24,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Portal",
-            dependencies: ["Studio"]),
+            dependencies: ["Suite"]),
     ]
 )
